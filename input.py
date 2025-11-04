b5cbe76b0647aee9459861458b647dd86c851b2f
Ishika Jain <jainishika654@gmail.com>
2025-11-04 21:49:42 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+# """hello
+# this is my 1st program"""
+
+# name=input("name:")
+# age=int(input("age:"))
+# print("my name is",name,"and i am",age ,"years old")
+
+
+
+# single line ternary operator
+food =input("food: ")
+eat="yes" if food=="cake" else "no"#both condn check in single line
+print(eat)
