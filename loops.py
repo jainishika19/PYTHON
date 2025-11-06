03723de7237fe635caec3a15e34cb338de886f78
Ishika Jain <jainishika654@gmail.com>
2025-11-06 19:10:35 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,15 @@
+# while loops
+# i=1
+# while i<=5:
+#     print(i*"*") #i will multiply with * and then prints i times times
+#     i=i+1
+
+# # //reverse pattern print
+# i=5
+# while i>=0:
+#     print(i*"*") #i will multiply with * and then prints i times times
+#     i=i-1
+
+# for loop
+for i in range(3): #as range starts from 0 and ends to 2,here 3 not included
+    print(i+1)
