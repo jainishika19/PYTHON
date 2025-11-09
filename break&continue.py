10ae09b377e544a4af25c3b686af746196665806
Ishika Jain <jainishika654@gmail.com>
2025-11-09 17:30:50 +0530

    Add files via upload

---
+++ break&continue.py
@@ -0,0 +1,14 @@
+# ----break----
+# students=["ram","shyam","radha","jiya"]
+# for student in students:
+#     if(student=="radha"):
+#         break #it stops the loop when radha comes
+#     print(student)
+
+
+# ---continue----
+students=["ram","shyam","radha","jiya"]
+for student in students:
+    if(student=="radha"):
+        continue #0nly skip radha 
+    print(student)
