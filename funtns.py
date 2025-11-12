9f6110fa60dbd6a2dbce4f3590ced540a4444965
Ishika Jain <jainishika654@gmail.com>
2025-11-12 17:59:41 +0530

    Add files via upload

---
+++ funtns.py
@@ -0,0 +1,57 @@
+# def hello():#fntn definition
+#     print("hello")
+#     #this fntn do not return anything
+
+# #fntn call
+# output=hello()
+# print(output)#it doesn't print anything as fntn do not return anything and we store it in output
+
+
+
+# def F(a,b):
+#     return a+b
+# output=F(2,3)#we store a+b result in it that is resturned by fntn
+# print(output) #now it print that is returned by fntn
+
+
+# print("hello",end=" ") #"end =" " " prints both words in same line 
+# print("world")
+
+
+
+
+                #    *****QUESTIONS******
+# 1) WAF to print the length of list (list is the paramter)
+cities=["delhi","jbp","indore"]
+def length(list):
+    print(len(list))
+length(cities) 
+
+
+# # 2)WAF to print the elements of a list in a single line (list in a parameter)
+# number=[1,2,3,4]
+# def print_list(list):
+#     for item in number:
+#         print(item,end=" ")
+
+# print_list(number)
+
+
+
+# 3)WAF to find the factorial of n (n is the parameter)
+
+# def factorial(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact=fact*i
+#     print(fact)
+# factorial(6)
+
+
+
+
+# # 4)WAF to convert USD into INR---1 USD=83 INR
+# def converter(usd_val):
+#     inr_val=usd_val*83
+#     print(usd_val,"USD=",inr_val,"INR")
+# converter(34)
