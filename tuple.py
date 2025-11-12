228fc354beec446793ac99e62ec78c3bd0208964
Ishika Jain <jainishika654@gmail.com>
2025-11-12 17:58:58 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,26 @@
+# #TUPLE()---immutable just like strings
+# person="abhi","jiya" #---note->by default it is tuple
+# print(type(person))
+
+
+# # tup=()#empty tuple
+# tup=(1,) #for store single value in tuple separate it by commmaa
+# tup1=(1)#it is not tuple
+# print(type(tup1))#prints int ,not tuple as it is considered as integrer value
+# # marks[0]=55 #error as element of tuple can't change --tuple is immutable
+# print(marks.count(5)) #count--how many times element is present in tuple
+# print(marks.index(5)) #indexing
+
+
+
+
+# ------------TUPLE METHODS---------
+tup=(1,2,1,4)
+print(tup.index(1)) #return index of element 2 at 1st occurence
+
+print(tup.count(1))
+
+
+
+
+
